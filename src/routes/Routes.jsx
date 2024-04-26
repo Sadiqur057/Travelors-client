64f900fb6ed6274f857632ddec94ba5110ecf20f
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2024-04-26 16:23:01 +0600

    added routes for login and register page

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Root from '../layout/Root';
-import Home from '../pages/Home';
+import Home from '../pages/Home/Home';
+import Login from '../pages/login/Login';
+import Register from '../pages/register/Register';
 
 const Routes = createBrowserRouter([
   {
@@ -11,6 +13,14 @@ const Routes = createBrowserRouter([
         path: '/',
         element:<Home></Home>
       },
+      {
+        path: '/login',
+        element: <Login></Login>
+      },
+      {
+        path: '/register',
+        element: <Register></Register>
+      }
 
       
     ]
