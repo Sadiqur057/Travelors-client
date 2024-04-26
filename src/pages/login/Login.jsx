c810163511c5970bd492372426c981cadba36026
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2024-04-26 16:25:01 +0600

    added functionality to authenticate user

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,26 +1,98 @@
-import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useContext, useState } from "react";
+import { AuthContext } from "../../providers/AuthProvider";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Helmet } from "react-helmet-async";
+import { VscEye, VscEyeClosed } from "react-icons/vsc";
+import { FaGithub, FaGoogle } from "react-icons/fa6";
 
 const Login = () => {
+  // scroll to top on load
+  const onLoad = () => {
+    window.scrollTo(0, 0);
+  };
+  onLoad();
+
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  // handle show password button
+  const [showPassword, setShowPassword] = useState(false);
+  const handleViewPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  // accessing auth context
+  console.log(location?.state);
+  const { loginUser, googleLogin, githubLogin } = useContext(AuthContext);
+
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
 
+  // social login
+  const handleGoogleLogin = () => {
+    googleLogin()
+      .then(() => {
+        toast.success("Login Success");
+        navigate(location?.state ? location.state : "/");
+        console.log(location?.state);
+      })
+      .catch((error) => {
+        toast.error(error);
+      });
+  };
+
+  const handleGithubLogin = () => {
+    githubLogin()
+      .then(() => {
+        toast.success("Login Success");
+        navigate(location?.state ? location.state : "/");
+      })
+      .catch((error) => {
+        toast.error(error);
+      });
+  };
+
+  // handle form submit
+  const onSubmit = (data, e) => {
+    e.preventDefault();
+    const email = data.email;
+    const password = data.password;
+    loginUser(email, password)
+      .then(() => {
+        toast.success("Login Success");
+        navigate(location?.state ? location.state : "/");
+      })
+      .catch((error) => {
+        const errorMsg = error.message.split("(")[1].split(")")[0];
+        if (errorMsg === "auth/invalid-credential") {
+          toast.error("Your email or password is incorrect");
+        } else {
+          toast.error(errorMsg);
+        }
+      });
+  };
+
   return (
     <section className=" lg:pt-6 flex py-[60px] items-center bg-cool">
-      {/* <Helmet>
-          <title>DH | Login</title>
-        </Helmet> */}
+      <Helmet>
+          <title>Travellors | Login</title>
+        </Helmet>
       <div className="flex justify-center w-5/6  md:w-fit  mx-auto bg-base-100 items-center mt-6  rounded-xl">
         <div className="flex flex-col justify-center text-center rounded-sm w-full  md:w-[400px]  lg:text-left p-0 flex-1">
           <div className="m-0 p-4 md:p-8 space-y-3 rounded-sm mx-auto lg:w-full lg:max-w-[400px] w-[90%]">
             <h1 className="text-3xl font-bold text-center pb-4">Login Here</h1>
-            <form noValidate="" action="" className="space-y-4">
-              <div className="space-y-4">
-                <div className="relative w-full min-w-[200px] h-10">
+            <form onSubmit={handleSubmit(onSubmit)}className="space-y-4">
+              <div className="flex flex-col">
+                <div className="relative w-full min-w-[200px] h-10 mb-2">
                   <input
+                    type="email"
+                    autoComplete="current-email"
                     className="peer w-full h-full bg-transparent text-blue-gray-700 font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2 border-t-transparent focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-teal-500"
                     placeholder=" "
                     {...register("email", { required: "Email is required" })}
@@ -29,19 +101,36 @@ const Login = () => {
                     Email
                   </label>
                 </div>
-                <div className="relative w-full min-w-[200px] h-10">
+                <p className="pb-2 text-left text-red-500">
+                  {errors.password?.message}
+                </p>
+                <div className="mt-1 relative w-full min-w-[200px] h-10">
                   <input
+                    type={showPassword ? "text" : "password"}
+                    autoComplete="current-password"
                     className="peer w-full h-full bg-transparent text-blue-gray-700 font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2 border-t-transparent focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-teal-500"
                     placeholder=" "
-                    {...register("password", { required: "Password is required" })}
+                    {...register("password", {
+                      required: "Password is required",
+                    })}
                   />
+
+                  <p
+                    onClick={handleViewPassword}
+                    className="cursor-pointer absolute right-4 top-3 text-xl"
+                  >
+                    {showPassword ? <VscEyeClosed /> : <VscEye />}
+                  </p>
+
                   <label className="flex w-full h-full select-none pointer-events-none absolute left-0 font-normal !overflow-visible truncate peer-placeholder-shown:text-blue-gray-500 leading-tight peer-focus:leading-tight peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500 transition-all -top-1.5 peer-placeholder-shown:text-sm text-[11px] peer-focus:text-[11px] before:content[' '] before:block before:box-border before:w-2.5 before:h-1.5 before:mt-[6.5px] before:mr-1 peer-placeholder-shown:before:border-transparent before:rounded-tl-md before:border-t peer-focus:before:border-t-2 before:border-l peer-focus:before:border-l-2 before:pointer-events-none before:transition-all peer-disabled:before:border-transparent after:content[' '] after:block after:flex-grow after:box-border after:w-2.5 after:h-1.5 after:mt-[6.5px] after:ml-1 peer-placeholder-shown:after:border-transparent after:rounded-tr-md after:border-t peer-focus:after:border-t-2 after:border-r peer-focus:after:border-r-2 after:pointer-events-none after:transition-all peer-disabled:after:border-transparent peer-placeholder-shown:leading-[3.75] text-blue-gray-400 peer-focus:text-teal-500 before:border-blue-gray-200 peer-focus:before:!border-teal-500 after:border-blue-gray-200 peer-focus:after:!border-teal-500">
                     Password
                   </label>
                 </div>
               </div>
+
               <input
                 type="submit"
+                
                 className="btn bg-c-primary  w-full text-white hover:bg-c-hover"
                 value="Login"
               />
@@ -58,28 +147,18 @@ const Login = () => {
                 aria-label="Login with Google"
                 type="button"
                 className="flex items-center justify-center w-full p-2 space-x-4 border rounded-md focus:ring-2 focus:ring-offset-1 border-c-primary text-c-primary"
+                onClick={handleGoogleLogin}
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 32 32"
-                  className="w-5 h-5 fill-current"
-                >
-                  <path d="M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.151-16 16s7.156 16 16 16c9.234 0 15.365-6.49 15.365-15.635 0-1.052-0.115-1.854-0.255-2.651z"></path>
-                </svg>
+                <FaGoogle className="text-lg"></FaGoogle>
                 <p>Login with Google</p>
               </button>
               <button
                 aria-label="Login with GitHub"
                 role="button"
                 className="flex items-center justify-center w-full p-2 space-x-4 border rounded-md focus:ring-2 focus:ring-offset-1 border-c-primary text-c-primary"
+                onClick={handleGithubLogin}
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 32 32"
-                  className="w-5 h-5 fill-current"
-                >
-                  <path d="M16 0.396c-8.839 0-16 7.167-16 16 0 7.073 4.584 13.068 10.937 15.183 0.803 0.151 1.093-0.344 1.093-0.772 0-0.38-0.009-1.385-0.015-2.719-4.453 0.964-5.391-2.151-5.391-2.151-0.729-1.844-1.781-2.339-1.781-2.339-1.448-0.989 0.115-0.968 0.115-0.968 1.604 0.109 2.448 1.645 2.448 1.645 1.427 2.448 3.744 1.74 4.661 1.328 0.14-1.031 0.557-1.74 1.011-2.135-3.552-0.401-7.287-1.776-7.287-7.907 0-1.751 0.62-3.177 1.645-4.297-0.177-0.401-0.719-2.031 0.141-4.235 0 0 1.339-0.427 4.4 1.641 1.281-0.355 2.641-0.532 4-0.541 1.36 0.009 2.719 0.187 4 0.541 3.043-2.068 4.381-1.641 4.381-1.641 0.859 2.204 0.317 3.833 0.161 4.235 1.015 1.12 1.635 2.547 1.635 4.297 0 6.145-3.74 7.5-7.296 7.891 0.556 0.479 1.077 1.464 1.077 2.959 0 2.14-0.020 3.864-0.020 4.385 0 0.416 0.28 0.916 1.104 0.755 6.4-2.093 10.979-8.093 10.979-15.156 0-8.833-7.161-16-16-16z"></path>
-                </svg>
+                <FaGithub className="text-lg"></FaGithub>
                 <p>Login with GitHub</p>
               </button>
             </div>
