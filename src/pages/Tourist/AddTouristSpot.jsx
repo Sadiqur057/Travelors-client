c6bbcf2cd04fd27bdc688006677e1c06193ac61e
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2024-04-28 20:48:14 +0600

    modified width of the form

--- src/pages/Tourist/AddTouristSpot.jsx
+++ src/pages/Tourist/AddTouristSpot.jsx
@@ -1,18 +1,29 @@
 import { useContext, useState } from "react";
 import { AuthContext } from "../../providers/AuthProvider";
 import { Select, Option, Input } from "@material-tailwind/react";
-import Swal from 'sweetalert2'
+import Swal from "sweetalert2";
 
 const AddTouristSpot = () => {
   const { user } = useContext(AuthContext);
   console.log(user?.displayName);
+  const userPhoto = user?.photoURL;
   const [seasonalityValue, setSeasonalityValue] = useState("");
+  const [countryValue, setCountryValue] = useState("");
   const [travelTimeValue, setTravelTimeValue] = useState();
 
   const seasonalityOptions = [
     { label: "Summer", value: "Summer" },
     { label: "Winter", value: "Winter" },
     { label: "All Time", value: "All Time" },
+  ];
+
+  const countryOptions = [
+    { label: "Bangladesh", value: "Bangladesh" },
+    { label: "Thailand", value: "Thailand" },
+    { label: "Indonesia", value: "Indonesia" },
+    { label: "Malaysia", value: "Malaysia" },
+    { label: "Vietnam", value: "Vietnam" },
+    { label: "Cambodia", value: "Cambodia" },
   ];
 
   const travelTimeOptions = [
@@ -31,6 +42,9 @@ const AddTouristSpot = () => {
   const handleTimeOptions = (value) => {
     setTravelTimeValue(value);
   };
+  const handleCountryOptions = (value) => {
+    setCountryValue(value);
+  };
 
   const handleAddSpot = (e) => {
     e.preventDefault();
@@ -39,53 +53,64 @@ const AddTouristSpot = () => {
     const email = form.email.value;
     const spot_name = form.spot_name.value;
     const visitors = form.visitors.value;
-    const country = form.country.value;
+    const country = countryValue;
     const location = form.location.value;
     const cost = form.cost.value;
     const description = form.description.value;
     const image = form.image.value;
     const seasonality = seasonalityValue;
     const travelTime = travelTimeValue;
-    const touristSpotDetails = {user, email, spot_name,  visitors,  country, location, description, seasonality, cost, travelTime,image}
+    const touristSpotDetails = {
+      user,
+      email,
+      spot_name,
+      visitors,
+      country,
+      location,
+      description,
+      seasonality,
+      cost,
+      travelTime,
+      image,
+      userPhoto,
+    };
     console.log(touristSpotDetails);
-    
-    fetch('http://localhost:5000/tourist-spots',{
-      method: "POST",
-      headers:{
-        'content-type':'application/json'
-      },
-      body: JSON.stringify(touristSpotDetails)
-    })
-    .then(res=>res.json())
-    .then(data=>{
-      console.log(data)
-      if(data.insertedId){
-        Swal.fire({
-          title: "Good job!",
-          text: "You clicked the button!",
-          icon: "success"
-        });
-      }
-    })
 
+    fetch("https://travelors-server.vercel.app/tourist-spots", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(touristSpotDetails),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.insertedId) {
+          Swal.fire({
+            title: "Success!",
+            text: "You have added the Tourist spot",
+            icon: "success",
+            confirmButtonColor: "#35a483",
+          });
+        }
+      });
   };
 
   return (
     <div className="bg-gray-400 py-10">
-      <section className="p-6 bg-white w-[90%] mx-auto rounded-md">
+      <section className="p-6 bg-white w-[90%] max-w-4xl mx-auto rounded-md">
+        <div className="space-y-2 col-span-full lg:col-span-1">
+          <p className="text-center font-bold text-2xl md:text-3xl py-8">
+            Add Your Favourite Tourist Spot
+          </p>
+        </div>
         <form
           onSubmit={handleAddSpot}
           className="container flex flex-col mx-auto space-y-12"
         >
-          <fieldset className="grid grid-cols-4 gap-6 rounded-md shadow-sm p-2 md:p-6 lg:p-10">
-            <div className="space-y-2 col-span-full lg:col-span-1">
-              <p className="font-bold text-3xl">Personal Inormation</p>
-              <p className="text-xs">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Adipisci fuga autem eum!
-              </p>
-            </div>
-            <div className="grid grid-cols-6 gap-4 col-span-full lg:col-span-3">
+          <fieldset className=" gap-6 rounded-md shadow-sm p-2 md:p-6 lg:p-10">
+            <div className="grid grid-cols-6 gap-4 col-span-full ">
               <div className="col-span-full sm:col-span-3">
                 <Input
                   label="Username"
@@ -105,13 +130,37 @@ const AddTouristSpot = () => {
                 />
               </div>
               <div className="col-span-full sm:col-span-3">
-                <Input label="Tourist Spot Name" color="teal" name="spot_name" />
+                <Input
+                  label="Tourist Spot Name"
+                  color="teal"
+                  name="spot_name"
+                />
               </div>
               <div className="col-span-full sm:col-span-3">
-                <Input label="Visitors Per Year" color="teal" name="visitors" type="number" />
+                <Input
+                  label="Visitors Per Year"
+                  color="teal"
+                  name="visitors"
+                  type="number"
+                />
               </div>
               <div className="col-span-full sm:col-span-3">
-                <Input label="Country Name" color="teal" name="country" />
+                <div>
+                  <Select
+                    label="Select Country"
+                    color="teal"
+                    onChange={handleCountryOptions}
+                  >
+                    {countryOptions.map((countryOption) => (
+                      <Option
+                        key={countryOption.value}
+                        value={countryOption.value}
+                      >
+                        {countryOption.label}
+                      </Option>
+                    ))}
+                  </Select>
+                </div>
               </div>
               <div className="col-span-full sm:col-span-3">
                 <Input label="Location" color="teal" name="location" />
@@ -125,14 +174,19 @@ const AddTouristSpot = () => {
                     placeholder=" "
                   ></textarea>
                   <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-green-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-green-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-green-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
-                    Textarea Green
+                    Description
                   </label>
                 </div>
               </div>
-              <div className="col-span-full sm:col-span-2">
-                <Input label="Average Cost" color="teal" name="cost" type="number" />
+              <div className="col-span-full lg:col-span-2">
+                <Input
+                  label="Average Cost"
+                  color="teal"
+                  name="cost"
+                  type="number"
+                />
               </div>
-              <div className="col-span-full sm:col-span-2">
+              <div className="col-span-full lg:col-span-2">
                 <div>
                   <Select
                     label="Select Seasonality"
@@ -150,7 +204,7 @@ const AddTouristSpot = () => {
                   </Select>
                 </div>
               </div>
-              <div className="col-span-full sm:col-span-2">
+              <div className="col-span-full lg:col-span-2">
                 <div>
                   <Select
                     label="Travel Time"
