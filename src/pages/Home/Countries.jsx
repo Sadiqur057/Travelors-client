f8d1895fb80d918a2ba7497418a45c68f68f7add
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2024-04-28 20:53:17 +0600

    added country section displaying countries from database

---
+++ src/pages/Home/Countries.jsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react";
+import Country from "./Country";
+
+const Countries = () => {
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    fetch("https://travelors-server.vercel.app/countries")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setCountries(data);
+      });
+  }, []);
+
+  return (
+    <section className=" body-font w-[90%] md:w-5/6 mx-auto ">
+      <div className="container py-14 mx-auto">
+        <div className="flex flex-col text-center w-full mb-6 md:mb-10">
+          <h1 className="sm:text-3xl text-2xl font-semibold title-font mb-4 ">
+            Master Cleanse Reliac Heirloom
+          </h1>
+          <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+            Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
+            gentrify, subway tile poke farm-to-table. Franzen you probably
+            havent heard of them man bun deep jianbing selfies heirloom.
+          </p>
+        </div>
+        <div className="md:grid grid-cols-7 grid-rows-12 gap-2 md:max-h-[700px] lg:max-h-[600px] min-h-[600px] space-y-3 md:space-y-0">
+          <div className="md:col-start-1 md:col-end-5 lg:col-start-1 lg:col-end-4 md:row-start-1 md:row-end-5 lg:row-start-1 lg:row-end-7">
+            <Country country={countries[0]}></Country>
+          </div>
+          <div className="md:col-start-5 md:col-end-8 lg:col-start-4 lg:col-end-6 lg:row-start-1 lg:row-end-7 row-start-1 row-end-5">
+            <Country country={countries[1]}></Country>
+          </div>
+          <div className=" lg:col-start-6 lg:col-end-8 lg:row-start-1 lg:row-end-7 col-start-1 col-end-4 row-start-5 row-end-9">
+            <Country country={countries[2]}></Country>
+          </div>
+          <div className="lg:col-start-1 lg:col-end-3 lg:row-start-7 lg:row-end-13 row-start-5 row-end-9 col-start-4 col-end-8">
+            <Country country={countries[3]}></Country>
+          </div>
+          <div className="lg:col-start-3 lg:col-end-5 lg:row-start-7 lg:row-end-13 col-start-1 col-end-5 row-start-9 row-end-13">
+            <Country country={countries[4]}></Country>
+          </div>
+          <div className="lg:col-start-5 lg:col-end-8 lg:row-start-7 lg:row-end-13 col-start-5 col-end-8  row-start-9 row-end-13">
+            <Country country={countries[5]}></Country>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Countries;
