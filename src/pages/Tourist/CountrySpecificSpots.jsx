ff2a4eac4d05e977c9934c60080c477572d3b770
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2024-04-28 22:01:17 +0600

    updated min height to screen

--- src/pages/Tourist/CountrySpecificSpots.jsx
+++ src/pages/Tourist/CountrySpecificSpots.jsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import TouristSpotCard from "../shared/TouristSpotCard";
+import { Spinner } from "@material-tailwind/react";
 
 const CountrySpecificSpots = () => {
+  const [loading, setLoading] = useState(true)
   const [touristSpots, setTouristSpots] = useState([]);
   const country = useParams();
   useEffect(() => {
@@ -11,8 +13,17 @@ const CountrySpecificSpots = () => {
       .then((data) => {
         console.log(data);
         setTouristSpots(data);
+        setLoading(false)
       });
   }, [country?.name]);
+
+  if (loading) {
+    return (
+      <div className="min-h-[calc(100vh-80px)] w-full flex justify-center items-center">
+        <Spinner className="h-12 w-12" color="teal" />
+      </div>
+    );
+  }
   return (
     <div className="w-[90%] md:w-5/6 mx-auto grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 my-6 md:my-10">
       {touristSpots.map((touristSpot) => (
