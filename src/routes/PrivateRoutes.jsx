21e8ab7d530680679d4f8f0b1fee805287959af1
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2024-04-28 20:56:49 +0600

    on reloading redicted to login page bug solved

--- src/routes/PrivateRoutes.jsx
+++ src/routes/PrivateRoutes.jsx
@@ -1,14 +1,19 @@
 import { useContext } from "react";
 import PropTypes from "prop-types";
 import { AuthContext } from "../providers/AuthProvider";
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 
 const PrivateRoutes = ({ children }) => {
-  const { user } = useContext(AuthContext);
+  const location = useLocation()
+  console.log(location.pathname)
+  const { user,loading } = useContext(AuthContext);
+  if(loading){
+    return <h1>Loading</h1>
+  }
   if (user) {
     return children;
   }
-  return <Navigate to="/"></Navigate>;
+  return <Navigate to="/login" state={location?.pathname}></Navigate>;
 };
 
 PrivateRoutes.propTypes = {
