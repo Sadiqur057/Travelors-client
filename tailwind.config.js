a158f13628a2654d33e287eba8d8b5b274556e90
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2024-04-29 16:47:21 +0600

    added more screen size to make more responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,39 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+const withMT = require("@material-tailwind/react/utils/withMT");
+ 
+module.exports = withMT({
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        'c-black': '#242c36',
+        'c-primary': '#35a483',
+        'c-hover':'#1a9a82',
+        'c-input':'#ebf6f28c',
+      },
+    },
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1180px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
   },
   plugins: [require("daisyui")],
-}
+  themes: ["light", "dark", "cupcake"],
+})
 
