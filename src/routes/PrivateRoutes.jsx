b6aa348943c0ca89d3374c44eaa62640c2de0cbd
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2024-04-29 16:44:55 +0600

    added loading spinner while data loading

--- src/routes/PrivateRoutes.jsx
+++ src/routes/PrivateRoutes.jsx
@@ -2,15 +2,21 @@ import { useContext } from "react";
 import PropTypes from "prop-types";
 import { AuthContext } from "../providers/AuthProvider";
 import { Navigate, useLocation } from "react-router-dom";
+import { Spinner } from "@material-tailwind/react";
 
 const PrivateRoutes = ({ children }) => {
-  const location = useLocation()
-  console.log(location.pathname)
-  const { user,loading } = useContext(AuthContext);
-  if(loading){
-    return <h1>Loading</h1>
+  const location = useLocation();
+  console.log(location.pathname);
+  const { user, loading } = useContext(AuthContext);
+  if (loading) {
+    return (
+      <div className="min-h-[calc(100vh-80px)] w-full flex justify-center items-center">
+        <Spinner className="h-12 w-12" color="teal" />
+      </div>
+    );
   }
   if (user) {
+    window.scrollTo(0, 0);
     return children;
   }
   return <Navigate to="/login" state={location?.pathname}></Navigate>;
