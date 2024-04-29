4e09202f2900a22db22a277d5ea55263112e9868
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2024-04-29 16:43:12 +0600

    added country name available spots and short description of the country at the top

--- src/pages/Tourist/CountrySpecificSpots.jsx
+++ src/pages/Tourist/CountrySpecificSpots.jsx
@@ -4,20 +4,33 @@ import TouristSpotCard from "../shared/TouristSpotCard";
 import { Spinner } from "@material-tailwind/react";
 
 const CountrySpecificSpots = () => {
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(true);
+  const [countryLoading, setCountryLoading] = useState(true);
   const [touristSpots, setTouristSpots] = useState([]);
+  const [countryInformation, setCountryInformation] = useState([]);
   const country = useParams();
+  window.scrollTo(0, 0);
   useEffect(() => {
     fetch(`https://travelors-server.vercel.app/countries/${country?.name}`)
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
         setTouristSpots(data);
-        setLoading(false)
+        setLoading(false);
       });
   }, [country?.name]);
 
-  if (loading) {
+  useEffect(() => {
+    fetch(`https://travelors-server.vercel.app/country/${country?.name}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setCountryInformation(data);
+        setCountryLoading(false);
+      });
+  }, [country?.name]);
+
+  if (loading || countryLoading) {
     return (
       <div className="min-h-[calc(100vh-80px)] w-full flex justify-center items-center">
         <Spinner className="h-12 w-12" color="teal" />
@@ -25,14 +38,33 @@ const CountrySpecificSpots = () => {
     );
   }
   return (
-    <div className="w-[90%] md:w-5/6 mx-auto grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 my-6 md:my-10">
-      {touristSpots.map((touristSpot) => (
-        <TouristSpotCard
-          key={touristSpot._id}
-          touristSpot={touristSpot}
-        ></TouristSpotCard>
-      ))}
-    </div>
+    <section className="text-gray-600 body-font pb-6 md:pb-10">
+      <div className="container px-5 py-8 mx-auto">
+        <div className="flex flex-wrap w-full mb-20 items-center">
+          <div className="lg:w-1/2 w-full mb-6 lg:mb-0">
+            <h1 className="sm:text-3xl text-2xl font-medium  mb-2 ">
+              {countryInformation.name}{" "}
+              <span className="text-base">
+                {" "}
+                Tourist spot available : {touristSpots.length}
+              </span>
+            </h1>
+            <div className="h-1 w-20 bg-c-primary rounded"></div>
+          </div>
+          <p className="lg:w-1/2 w-full leading-relaxed text-gray-500">
+            {countryInformation.description}
+          </p>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 my-6 md:my-10">
+          {touristSpots.map((touristSpot) => (
+            <TouristSpotCard
+              key={touristSpot._id}
+              touristSpot={touristSpot}
+            ></TouristSpotCard>
+          ))}
+        </div>
+      </div>
+    </section>
   );
 };
 
