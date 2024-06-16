a989ace48560522e1edcae202e74467178f78356
sadiqur057 <sadiqur057@gmail.com>
2024-06-16 16:45:53 +0600

    updated

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,15 +4,7 @@ import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyB9xr2O8_HBP_zUeWLUrzpH-WiSjDRwVRo",
-//   authDomain: "travellors-bd.firebaseapp.com",
-//   projectId: "travellors-bd",
-//   storageBucket: "travellors-bd.appspot.com",
-//   messagingSenderId: "262902445281",
-//   appId: "1:262902445281:web:60084ba5c8b822f2d3f3a0"
-// };
+
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_APIKEY,
